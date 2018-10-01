69202e65e6a03410bea92eedd2b6e29f031c0475
goutham16 <43151756+goutham16@users.noreply.github.com>
2018-10-01 10:47:46 -0400

    Create Assn2_StockList_Students_3.cs

---
+++ Assn2_StockList_Students_3.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+  public partial class StockList
+  {
+    //param        : NA
+    //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+    //return       : total value
+    //return type  : decimal
+    public decimal Value()
+    {
+      decimal value = 0.0m;
+            Stock stock = new Stock();
+
+            // write your implementation here
+            value = stock.Holdings * stock.CurrentPrice;
+
+      return value;
+    }
+
+    //param  (StockList) listToCompare     : StockList which has to comared for similarity index
+    //summary      : finds the similar number of nodes between two lists
+    //return       : similarty index
+    //return type  : int
+    public int Similarity(StockList listToCompare)
+    {
+      int similarityIndex = 0;
+
+            // write your implementation here
+
+            StockList stocklist = new StockList();
+
+
+
+            while (stocklist.head != null && listToCompare.head != null && stocklist.head.StockHolding == listToCompare.head.StockHolding)
+            {
+                stocklist.head = stocklist.head.Next;
+                listToCompare.head = listToCompare.head.Next;
+                similarityIndex++;
+            }
+
+            
+
+
+            return similarityIndex;
+    }
+
+    //param        : NA
+    //summary      : Print all the nodes present in the list
+    //return       : NA
+    //return type  : NA
+    public void Print()
+    {
+            // write your implementation here
+
+
+            StockNode current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.StockHolding);
+                current = current.Next;
+            }
+
+            
+
+        }
+  }
+}
